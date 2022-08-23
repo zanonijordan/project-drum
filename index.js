6e70d3c591dc14ce098edf91f7b7820d7481718f
JORDAN HENRIQUE MOREIRA ZANONI <52969396+zanonijordan@users.noreply.github.com>
2022-08-23 19:12:57 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+var numberDrums = document.querySelectorAll(".drum").length
+
+for (i = 0; i < numberDrums; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener('click', function() {
+    let buttonInnerHTML = this.innerHTML
+    makeSound(buttonInnerHTML)
+    bottonAnimation(buttonInnerHTML)
+
+  });
+}
+document.addEventListener('keydown', function(event){
+  makeSound(event.key)
+  bottonAnimation(event.key)
+})
+
+function makeSound(key){
+  switch (key) {
+    case 'w':
+    let tom1 = new Audio('./sounds/tom-1.mp3')
+    tom1.play();
+      break;
+    case 'a':
+    let tom2 = new Audio('./sounds/tom-2.mp3')
+    tom2.play();
+      break;
+    case 's':
+    let tom3 = new Audio('./sounds/tom-3.mp3')
+    tom3.play();
+      break;
+    case 'd':
+    let tom4 = new Audio('./sounds/tom-4.mp3')
+    tom4.play();
+      break;
+    case 'j':
+    let snare = new Audio('./sounds/snare.mp3')
+    snare.play();
+      break;
+    case 'k':
+    let crash = new Audio('./sounds/crash.mp3')
+    crash.play();
+      break;
+    case 'l':
+    let kickBass = new Audio('./sounds/kick-bass.mp3')
+    kickBass.play();
+      break;
+    default:
+  }
+}
+function bottonAnimation(currentKey){
+  let activeButton = document.querySelector('.'+ currentKey)
+  activeButton.classList.add('pressed')
+  setTimeout(function() {
+    activeButton.classList.remove('pressed')
+  },100)
+}
